68b98e6aa5d966e1b029087504f4584112ea2335
john-melin <paragateoslo@gmail.com>
2023-01-13 13:14:19 +0100

    Move css

--- src/Game/PixelOverlay.tsx
+++ src/Game/PixelOverlay.tsx
@@ -8,15 +8,6 @@ const PixelOverlay = () => {
   return (
     <div
       style={{
-        position: 'absolute',
-        'pointer-events': 'none',
-        gap: '1px',
-        background:
-          'radial-gradient(circle, rgba(48,48,48,1) 0%, rgba(255,255,255,1) 20%, rgba(119,119,119,1) 44%, rgba(164,164,164,1) 77%, rgba(255,255,255,1) 100%)',
-        display: 'grid',
-        height: '100%',
-        width: '100%',
-        opacity: '0.03',
         'grid-template-columns': `repeat(${pixelsPerRow}, minmax(0, 1fr))`,
         'grid-template-rows': `repeat(${pixelsPerRow}, minmax(0, 1fr))`,
       }}
@@ -24,7 +15,7 @@ const PixelOverlay = () => {
       {pixelRow.map(() => (
         <>
           {pixelRow.map(() => (
-            <div style={{ height: '100%', width: '100%', border: '1px solid black' }}></div>
+            <div class="pixel-cell"></div>
           ))}
         </>
       ))}
