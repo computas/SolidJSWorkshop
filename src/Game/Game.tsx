5822dc2d62389b06cc664804b6305149d317fc2f
john-melin <paragateoslo@gmail.com>
2023-01-14 12:34:52 +0100

    Remove keyboard handling

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -1,4 +1,4 @@
-import { createSignal, createRenderEffect } from 'solid-js';
+import { createSignal } from 'solid-js';
 import GameConfig from '../game-config';
 import Grid from '../Grid/Grid';
 import { getHead } from '../helpers/get-body-part';
@@ -12,18 +12,6 @@ export default () => {
   const [direction, setDirection] = createSignal(getHead(GameConfig.initSnake).direction);
 
   setInterval(() => moveSnake(), GameConfig.initSpeed);
-
-  createRenderEffect(() => {
-    document.body.addEventListener('keydown', (event) => handleKey(event.key));
-  });
-
-  function handleKey(key: string) {
-    const head = getHead(snake());
-    if (key === 'ArrowRight' && head.direction !== 'left') setDirection('right');
-    if (key === 'ArrowLeft' && head.direction !== 'right') setDirection('left');
-    if (key === 'ArrowUp' && head.direction !== 'down') setDirection('up');
-    if (key === 'ArrowDown' && head.direction !== 'up') setDirection('down');
-  }
 
   function getMovedHead(part: SnakeBodyPart) {
     const x = part.x + XMod[direction()];
